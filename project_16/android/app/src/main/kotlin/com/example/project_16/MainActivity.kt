dc1db51eec3eaadfbad4b7cab25c0b6ce9f21c48
Abhi <ak6063581@gmail.com>
2024-09-24 08:42:03 +0530

    new code

--- project_16/android/app/src/main/kotlin/com/example/project_16/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.project_16
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ sign_language/lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("honinsta"),
+      ),
+      body: Column(
+        children: [
+          Row(
+            height:
+          ),
+        ],
+      ),
+    );
+  }
+}
